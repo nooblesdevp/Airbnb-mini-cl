525b79f9a9d5010f21be864c67c55610ae68b6f8
nuzulzen <nuzulzen@gmail.com>
2020-10-20 21:48:01 +0700

    add react-router-dom nd footer

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "./react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import "./App.scss";
 import Banner from "./Components/Banner/Banner";
 import Footer from "./Components/Footer/Footer";
 import Header from "./Components/Header/Header";
 import Home from "./Components/Home/Home";
+import SearchPage from "./Components/SearchPage/SearchPage";
 
 function App() {
   return (
     //BEM
     <div className="app">
-      {/* Header */}
-      <Header />
-      {/* Home */}
-      <Home />
-      {/* footer */}
-      <Footer />
+      <Router>
+        <Header />
+
+        <Switch>
+          <Route path="/search">
+            <SearchPage />
+          </Route>
+
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+
+        <Footer />
+      </Router>
     </div>
   );
 }
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,17 +1,22 @@
 import React from "react";
-import { ExpandMoreOutlined, Language, Search } from "@material-ui/icons";
+import { Link } from "react-router-dom";
 
 import "./Header.scss";
+
 import { Avatar } from "@material-ui/core";
+import { ExpandMoreOutlined, Language, Search } from "@material-ui/icons";
 
 function Header() {
   return (
     <div className="header">
-      <img
-        className="header__icon"
-        src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png"
-        alt=""
-      />
+      <Link to="/">
+        <img
+          className="header__icon"
+          src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png"
+          alt=""
+        />
+      </Link>
+
       <div className="header__center">
         <input type="text" />
         <Search className="header__icon" />
